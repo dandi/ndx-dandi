07dbeef7d05623b33792e99f9a391d73a922d334
Yaroslav Halchenko <debian@onerussian.com>
2020-02-05 14:12:13 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# ndx-dandi Extension for NWB:N
---
+++ setup.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+from shutil import copy2
+
+setup_args = {
+    'name': 'ndx-dandi',
+    'version': '0.1.0',
+    'description': 'DANDI project extension',
+    'author': 'Yaroslav O Halhenko',
+    'author_email': 'debian@onerussian.com',
+    'url': '',
+    'license': 'BSD 3-Clause',
+    'install_requires': [
+        'pynwb>=1.1.2'
+    ],
+    'packages': find_packages('src/pynwb'),
+    'package_dir': {'': 'src/pynwb'},
+    'package_data': {'ndx_dandi': [
+        'spec/ndx-dandi.namespace.yaml',
+        'spec/ndx-dandi.extensions.yaml',
+    ]},
+    'classifiers': [
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+    ],
+    'zip_safe': False
+}
+
+
+def _copy_spec_files(project_dir):
+    ns_path = os.path.join(project_dir, 'spec', 'ndx-dandi.namespace.yaml')
+    ext_path = os.path.join(project_dir, 'spec', 'ndx-dandi.extensions.yaml')
+
+    dst_dir = os.path.join(project_dir, 'src', 'pynwb', 'ndx_dandi', 'spec')
+    if not os.path.exists(dst_dir):
+        os.mkdir(dst_dir)
+
+    copy2(ns_path, dst_dir)
+    copy2(ext_path, dst_dir)
+
+
+if __name__ == '__main__':
+    _copy_spec_files(os.path.dirname(__file__))
+    setup(**setup_args)
